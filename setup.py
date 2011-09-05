5b37e32c25c90cfec779ff9eeb5c8be91c10c23a
Jason Ford <jason@jason-ford.com>
2011-09-05 16:40:42 -0500

    Fixed version number in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 from pyrise import __version__
 
 setup(name="pyrise",
-      version="0.2",
+      version=__version__,
       description="Python wrapper for 37Signals Highrise",
       long_description="A work in progress, but one that will be awesome when finished. Pyrise gives you class objects that work a lot like Django models, making the whole experience of integrating with Highrise just a little more awesome and Pythonic.",
       license="MIT License",
