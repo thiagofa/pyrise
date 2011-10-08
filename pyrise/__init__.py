ee6be30475ab40f4e4116e5db2b5942fc247d2a5
Jason Ford <jason@jason-ford.com>
2011-10-08 17:29:13 -0500

    Minor version bump.

--- pyrise/__init__.py
+++ pyrise/__init__.py
@@ -5,7 +5,7 @@ import sys
 from datetime import datetime, timedelta
 from xml.etree import ElementTree
 
-__version__ = '0.3.2'
+__version__ = '0.3.3'
 
 class Highrise:
     """Class designed to handle all interactions with the Highrise API."""
